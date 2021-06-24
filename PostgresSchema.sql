f2d47e5802fa18356ca1d14af17893d2a91a5986
rohitshantarampatil <rohitpatil41199@gmail.com>
2021-06-24 08:56:30 +0530

    ER updated

--- PostgresSchema.sql
+++ PostgresSchema.sql
@@ -5,7 +5,7 @@ CREATE TABLE users(
     userEmail varchar(50) NOT NULL,
     password varchar(50) NOT NULL,
     userName varchar(50) NOT NULL,
-    dateOfCreation TIMESTAMP DEFAULT current_timestamp,
+    createdAt TIMESTAMP DEFAULT current_timestamp,
     gender varchar(10),
     userRating decimal(2,1),
     PRIMARY KEY (userId),
