1227d5f66c3ae0ebf2ad65ad1e1155d0f7cfa2eb
rohitshantarampatil <rohitpatil41199@gmail.com>
2021-06-24 16:36:05 +0530

    main restored

--- PostgresSchema.sql
+++ PostgresSchema.sql
@@ -3,11 +3,10 @@ CREATE DATABASE project;
 CREATE TABLE users(
     userId SERIAL,
     userName varchar(50) NOT NULL,
-    email varchar(50) NOT NULL,
+    email varchar(50) NOT NULL UNIQUE,
     password varchar(50) NOT NULL,
     gender varchar(10),
     rating decimal(2,1),
     createdAt TIMESTAMP DEFAULT current_timestamp,
-    PRIMARY KEY (id),
-    UNIQUE (email)
+    PRIMARY KEY (userId)
 );
--- progressive/postgres/src/main/java/com/sapient/postgres/App.java
+++ progressive/postgres/src/main/java/com/sapient/postgres/App.java
@@ -6,12 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.sapient.beans.User;
+import com.sapient.dao.UserDAO;
 import com.sapient.utils.GetConnection;
+
+import jdk.internal.jline.internal.Log;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Hello world!
  *
  */
+@Slf4j
 public class App 
 {
     public static void main( String[] args )
@@ -20,24 +25,31 @@ public class App
         Connection conn = GetConnection.getPostGressConn();
         System.out.println(conn);
 		
-		List<User> list = null;
-		try {
-			String sql1 = "SELECT * FROM users";
-			GetConnection gc = new GetConnection();
-			gc.ps = GetConnection.getPostGressConn().prepareStatement(sql1);
-			list = new ArrayList<User>();
-			
-			gc.rs = gc.ps.executeQuery(); 
-			while(gc.rs.next()) {
-				User emp = new User(); 
-				emp.setUserId(gc.rs.getInt(1));
-				emp.setUserName(gc.rs.getString(2));
-				list.add(emp); 
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		for(User u:list) System.out.println(u);
-		
+//		List<User> list = null;
+//		try {
+//			
+//			String sql1 = "SELECT * FROM users";
+//			GetConnection gc = new GetConnection();
+//			gc.ps = GetConnection.getPostGressConn().prepareStatement(sql1);
+//			list = new ArrayList<User>();
+//			
+//			gc.rs = gc.ps.executeQuery(); 
+//			while(gc.rs.next()) {
+//				User emp = new User(); 
+//				emp.setUserId(gc.rs.getInt(1));
+//				emp.setUserName(gc.rs.getString(2));
+//				list.add(emp); 
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		for(User u:list) System.out.println(u);
+//		
+        
+        User user = new User(null, "user", null, null, null, null, null);
+
+        log.info(user.toString());
+//        UserDAO dao = new UserDAO();
+//        dao.insertUser(user);
     }
 }
